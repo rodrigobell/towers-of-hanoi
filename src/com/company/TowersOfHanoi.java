1ce75426bf5bdede2c939aa21b1eab34b373fb37
Rodrigo Bell <rb-mac@sfs-wifi-dhcp-10-143-170-87.wifi.sfsu.edu>
2017-01-28 17:13:38 -0800

    Created Peg class and worked on solve method

--- src/com/company/TowersOfHanoi.java
+++ src/com/company/TowersOfHanoi.java
@@ -1,7 +1,18 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TowersOfHanoi {
+
+	public void solve(int n, Peg start, Peg aux, Peg finish) {
+		if (n == 1) {
+			System.out.println(n + " from " + start.name + " to " + finish.name);
+		} else {
+			solve(n - 1, start, aux, finish);
+			System.out.println(n + " from " + start.name + " to " + finish.name);
+			solve(n - 1, aux, start, finish);
+		}
+	}
 
 //	public void solve(int n, String start, String auxiliary, String end) {
 //		if (n == 1) {
@@ -14,11 +25,40 @@ public class TowersOfHanoi {
 //	}
 
     public static void main(String[] args) {
-    	TowersOfHanoi towers = new TowersOfHanoi();
-	    System.out.print("Enter number of disks: ");
-	    Scanner scanner = new Scanner(System.in);
-	    int disks = scanner.nextInt();
-	    System.out.print(disks);
+		System.out.println("Move                   Peg Configuration");
+		System.out.println("                       A          B          C");
 
+//	    System.out.print("Enter number of disks: ");
+//	    Scanner scanner = new Scanner(System.in);
+//	    int disks = scanner.nextInt();
+
+		int disks = 3;
+
+	    Peg A = new Peg("A");
+	    Peg B = new Peg("B");
+	    Peg C = new Peg("C");
+
+		TowersOfHanoi towers = new TowersOfHanoi();
+
+	    towers.solve(disks, A, B, C);
     }
+}
+
+class Peg {
+	public String name = new String();
+//	private ArrayList<Integer> disks = new ArrayList<Integer>();
+
+	public Peg() {}
+
+	public Peg(String name) {
+		this.name = name;
+	}
+
+//	public void Peg(String name, int n) {
+//		this.name = name;
+//
+//		for (int i = 0; i < n; i++) {
+//			this.disks.add(n);
+//		}
+//	}
 }

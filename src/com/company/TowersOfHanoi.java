6a85a6ddd6d7441bb9a7251e7c7e8c433bb8c8f4
Rodrigo Bell <rb-mac@sfs-wifi-dhcp-10-143-170-87.wifi.sfsu.edu>
2017-01-29 20:26:03 -0800

    Solved challenge and formatted according to sample output

--- src/com/company/TowersOfHanoi.java
+++ src/com/company/TowersOfHanoi.java
@@ -1,52 +1,55 @@
 package com.company;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class TowersOfHanoi {
+	public int nDisks = 3;
+	public Peg A = new Peg("A", nDisks);
+	public Peg B = new Peg("B");
+	public Peg C = new Peg("C");
 
-	public void solve(int n, Peg start, Peg aux, Peg finish) {
-		if (n == 1) {
-			System.out.println(n + " from " + start.name + " to " + finish.name);
+	public void start() {
+		System.out.println("Move                   Peg Configuration");
+		System.out.println("                    A         B         C");
+		System.out.print("init                ");
+		for (int i = nDisks; i > 0; i--) {
+			System.out.print(i);
+		}
+		System.out.print('\n');
+		solve(nDisks, A, B, C);
+	}
+
+	public void solve(int topDisk, Peg A, Peg B, Peg C) {
+		if (topDisk == 1) {
+			int d = A.pop();
+			C.push(d);
+			System.out.print(d + " from " + A.name + " to " + C.name + "       ");
+			printStep();
 		} else {
-			solve(n - 1, start, aux, finish);
-			System.out.println(n + " from " + start.name + " to " + finish.name);
-			solve(n - 1, aux, start, finish);
+			solve(topDisk - 1, A, C, B);
+			int d = A.pop();
+			C.push(d);
+			System.out.print(d + " from " + A.name + " to " + C.name + "       ");
+			printStep();
+			solve(topDisk - 1, B, A, C);
 		}
 	}
 
-//	public void solve(int n, String start, String auxiliary, String end) {
-//		if (n == 1) {
-//			System.out.println(start + " -> " + end);
-//		} else {
-//			solve(n - 1, start, end, auxiliary);
-//			System.out.println(start + " -> " + end);
-//			solve(n - 1, auxiliary, start, end);
-//		}
-//	}
+	public void printStep() {
+		this.A.printDisks(nDisks);
+		this.B.printDisks(nDisks);
+		this.C.printDisks(nDisks);
+		System.out.print('\n');
+	}
 
     public static void main(String[] args) {
-		System.out.println("Move                   Peg Configuration");
-		System.out.println("                       A          B          C");
-
-//	    System.out.print("Enter number of disks: ");
-//	    Scanner scanner = new Scanner(System.in);
-//	    int disks = scanner.nextInt();
-
-		int disks = 3;
-
-	    Peg A = new Peg("A");
-	    Peg B = new Peg("B");
-	    Peg C = new Peg("C");
-
-		TowersOfHanoi towers = new TowersOfHanoi();
-
-	    towers.solve(disks, A, B, C);
+		TowersOfHanoi tower = new TowersOfHanoi();
+		tower.start();
     }
 }
 
 class Peg {
 	public String name = new String();
-//	private ArrayList<Integer> disks = new ArrayList<Integer>();
+	public ArrayList<Integer> disks = new ArrayList<Integer>();
 
 	public Peg() {}
 
@@ -54,11 +57,35 @@ class Peg {
 		this.name = name;
 	}
 
-//	public void Peg(String name, int n) {
-//		this.name = name;
-//
-//		for (int i = 0; i < n; i++) {
-//			this.disks.add(n);
-//		}
-//	}
+	public Peg(String name, int n) {
+		this.name = name;
+
+		for (int i = n; i > 0; i--) {
+			this.disks.add(i);
+		}
+	}
+
+	void push(int d) {
+		this.disks.add(d);
+	}
+
+	int pop() {
+		int d = 0;
+		if (!this.disks.isEmpty()) {
+			d = this.disks.get(this.disks.size() - 1);
+			this.disks.remove(this.disks.size() - 1);
+		}
+
+		return d;
+	}
+
+	void printDisks(int nDisks) {
+		for (int i = 0; i < nDisks; i++) {
+			if (i >= this.disks.size())
+				System.out.print(" ");
+			else
+				System.out.print(this.disks.get(i));
+		}
+		System.out.print("       ");
+	}
 }
